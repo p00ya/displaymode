1bef49c51687406305d1e5dc88bf9044a88bc97f
Dean Scarff <dos@scarff.id.au>
2022-02-25 18:46:39 +1100

    Bump to version 1.3.3
    No logic changes; but this version will be built for Apple Silicon as
    well as Intel.

--- displaymode.c
+++ displaymode.c
@@ -1,6 +1,6 @@
 // displaymode - a utility for changing the display resolution on Mac OS X.
 //
-// Copyright 2019, 2020, 2021 Dean Scarff.
+// Copyright 2019, 2020, 2021, 2022 Dean Scarff.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@
 #include <MacTypes.h>
 
 // Name and version to display with "v" option.
-const char kProgramVersion[] = "displaymode 1.3.2";
+const char kProgramVersion[] = "displaymode 1.3.3";
 
 // States for the main invocation "option".
 //
